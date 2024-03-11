a28ffdda4734dc4dc5563247874896df8e0eed07
Fadzaa <fadza20@gmail.com>
2024-03-12 00:39:35 +0700

    utils: [FA] add asset & color pallete

---
+++ lib/common/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color baseColor = Color(0xFFFAFAFA);
+const Color primaryColor = Color(0xFF6149C3);
+const Color blackColor = Color(0xFF1D1D1D);
+const Color greyColor = Color(0xFF707070);
+const Color secondaryColor = Color(0xFFF654AE);
+const Color warningColor = Color(0xFFFFC700);
+const Color dangerColor = Color(0xFFFB3900);
