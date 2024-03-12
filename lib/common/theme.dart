5d5633568523dc379e4cf72ba4381f67d4c5142e
Fadzaa <fadza20@gmail.com>
2024-03-12 14:58:26 +0700

    utils: [FA] add textstyle

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -1,6 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// final ThemeData appTheme = ThemeData(
+//   primaryColor: primaryColor,
+//   accentColor: secondaryColor,
+//   scaffoldBackgroundColor: baseColor,
+//   fontFamily: GoogleFonts.poppins().fontFamily,
+//   appBarTheme: AppBarTheme(
+//     elevation: 0,
+//     color: baseColor,
+//     iconTheme: IconThemeData(
+//       color: blackColor,
+//     ),
+//   ),
+//   textTheme: TextTheme(
+//     headline1: GoogleFonts.poppins(
+//       fontSize: 24,
+//       fontWeight: FontWeight.w600,
+//       color: blackColor,
+//     ),
+//     headline2: GoogleFonts.poppins(
+//       fontSize: 20,
+//       fontWeight: FontWeight.w600,
+//       color: blackColor,
+//     ),
+//     headline3: GoogleFonts.poppins(
+//       fontSize: 18,
+//       fontWeight: FontWeight.w600,
+//       color: blackColor,
+//     ),
+//     headline4: GoogleFonts.poppins(
+//       fontSize: 16,
+//       fontWeight: FontWeight.w600,
+//       color: blackColor,
+//     ),
+//     headline5: GoogleFonts.poppins(
+//       fontSize: 14,
+//       fontWeight: FontWeight.w600,
+//       color: blackColor,
+//     ),
+//     headline6: GoogleFonts.poppins(
+//       fontSize: 12,
+//       fontWeight: FontWeight.w600,
+//       color: blackColor,
+//     ),
+//     bodyText1: GoogleFonts.poppins(
+//       fontSize: 14,
+//       fontWeight: FontWeight.w400,
+//       color: blackColor,
+//     ),
+//     bodyText2: GoogleFonts.poppins(
+//       fontSize: 12,
+//       fontWeight: FontWeight.w400,
+//       color: blackColor,
+//     ),
+//     subtitle1: GoogleFonts.poppins(
+//       fontSize: 14,
+//       fontWeight: FontWeight.w600,
+//       color: blackColor,
+//     ),
+//     subtitle2: GoogleFonts.poppins(
+//       fontSize: 12,
+//       fontWeight: FontWeight.w600,
+//       color: blackColor,
+//     ),
+//     button: GoogleFonts.poppins(
+//       fontSize: 14,
+//       fontWeight: FontWeight.w600,
+//       color: baseColor,
+//     ),
+//   ),
+// );
+
 const Color baseColor = Color(0xFFFAFAFA);
 const Color primaryColor = Color(0xFF6149C3);
 const Color blackColor = Color(0xFF1D1D1D);
@@ -8,3 +79,83 @@ const Color greyColor = Color(0xFF707070);
 const Color secondaryColor = Color(0xFFF654AE);
 const Color warningColor = Color(0xFFFFC700);
 const Color dangerColor = Color(0xFFFB3900);
+
+//HEADING
+const TextStyle tsHeadingLarge = TextStyle(
+  fontSize: 28,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+const TextStyle tsHeadingMedium = TextStyle(
+  fontSize: 26,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+const TextStyle tsHeadingSmall = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+//TITLE
+const TextStyle tsTitleLarge = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+const TextStyle tsTitleMedium = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+const TextStyle tsTitleSmall = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+//BODY
+const TextStyle tsBodyLarge = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+const TextStyle tsBodyMedium = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+const TextStyle tsBodySmall = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+//LABEL
+
+const TextStyle tsLabelLarge = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+const TextStyle tsLabelMedium = TextStyle(
+  fontSize: 8,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+const TextStyle tsLabelSmall = TextStyle(
+  fontSize: 6,
+  fontWeight: FontWeight.normal,
+  color: blackColor,
+);
+
+
+
