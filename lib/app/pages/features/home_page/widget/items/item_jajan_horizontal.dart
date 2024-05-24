8f0756ece4ad8c8e720105b3c297c4a302c3c266
SyahranFD <fadhilrafa1@gmail.com>
2024-05-24 21:50:59 +0700

    feat: [SF] add gradient in item jajan horizontal and vertical

--- lib/app/pages/features/home_page/widget/items/item_jajan_horizontal.dart
+++ lib/app/pages/features/home_page/widget/items/item_jajan_horizontal.dart
@@ -19,72 +19,92 @@ class ItemJajanHorizontal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    return Container(
-      width: width * 0.40,
-      height: width * 0.42,
-      margin: const EdgeInsets.only(right: 10),
-      padding: const EdgeInsets.all(13),
-      decoration: BoxDecoration(
-        image: DecorationImage(image: NetworkImage(image), fit: BoxFit.cover),
-        borderRadius: BorderRadius.circular(16),
+    return Stack(
+      children: [
+        Container(
+          width: width * 0.40,
+          height: width * 0.42,
+          margin: const EdgeInsets.only(right: 10),
+          padding: const EdgeInsets.all(13),
+          decoration: BoxDecoration(
+            image: DecorationImage(image: NetworkImage(image), fit: BoxFit.cover),
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
 
-
-      ),
-
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-        children: [
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 4),
-                decoration: BoxDecoration(
-                  color: secondaryColor,
-                  borderRadius: BorderRadius.circular(20)
-                ),
-                child: Center(
-                  child: Row(
-                    children: [
-
-                      SvgPicture.asset(icStar),
-
-                      const SizedBox(width: 5,),
-
-                      Text(rating.toString(),
-                        style: tsBodySmall.copyWith(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600
-                        ),
-                      )
-
-                    ],
-                  ),
-                ),
-              ),
-            ],
+        Container(
+          width: width * 0.40,
+          height: width * 0.42,
+          margin: const EdgeInsets.only(right: 10),
+          padding: const EdgeInsets.all(13),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              colors: [
+                Colors.black.withOpacity(0.5),
+                Colors.transparent,
+              ],
+            ),
           ),
 
-          Row(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
+
             children: [
-              Flexible(
-                child: Text(
-                  name,
-                  style: tsBodySmall.copyWith(fontWeight: FontWeight.w600, color: Colors.white),
-                ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 4),
+                    decoration: BoxDecoration(
+                        color: secondaryColor,
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: Center(
+                      child: Row(
+                        children: [
+
+                          SvgPicture.asset(icStar),
+
+                          const SizedBox(width: 5,),
+
+                          Text(rating.toString(),
+                            style: tsBodySmall.copyWith(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600
+                            ),
+                          )
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Text(
+                      name,
+                      style: tsBodySmall.copyWith(fontWeight: FontWeight.w600, color: Colors.white),
+                    ),
+                  ),
 
-              isHalal ? Image.asset(icHalal) : Container()
 
+                  isHalal ? Image.asset(icHalal) : Container()
+
+                ],
+              )
             ],
-          )
-        ],
-      ),
+          ),
+        ),
+      ],
     );
   }
 }
